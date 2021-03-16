710d5f7afb56a9c5048ad48d01fcea30cbd664f7
meleciosilva <mpsilva94@gmail.com>
2021-03-16 09:22:22 -0400

    added create deck button

---
+++ src/Home/CreateDeckButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CreateDeckButton() {
+  return (
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="button" class="btn btn-secondary mb-1"><strong>+</strong> Create Deck</button>
+      </div>
+    </div>  
+  )
+}
+
+export default CreateDeckButton;
