1f1cfd0d520c2819be95b7e389751268aefd7f64
meleciosilva <mpsilva94@gmail.com>
2021-03-17 12:04:00 -0400

    added window.confirm to restart or return home

--- src/Study/DisplayCard.js
+++ src/Study/DisplayCard.js
@@ -4,15 +4,18 @@ import {Link} from "react-router-dom";
 function DisplayCard({deckCards}) {
   
   const [Card, setCard] = useState(0);
-  const [side, setSide] = useState(true);
+  const [Side, setSide] = useState(true);
 
   function handleFlip() {
-    setSide(!side);
+    setSide(!Side);
   }
 
   function handleNext() {
     setCard(prevCard => prevCard + 1);
     setSide(true); // sets side to front when switching to another card
+    if (Card === deckCards.length - 1) {
+      return window.confirm("Restart Cards?") ? setCard(0) : window.open("/", "Home");
+    }
   }
 
   function handlePrev() {
@@ -26,7 +29,7 @@ function DisplayCard({deckCards}) {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Card {Card + 1} of {deckCards.length}</h5>
-            <p className="card-text">{side ? deckCards[Card].front : deckCards[Card].back}</p>
+            <p className="card-text">{Side ? deckCards[Card].front : deckCards[Card].back}</p>
             <Link id="previous" className={!Card ? "btn btn-primary disabled" : "btn btn-primary"} onClick={handlePrev}>Previous</Link>
             <Link className="btn btn-secondary mx-2" onClick={handleFlip}>Flip</Link>
             <Link className="btn btn-primary" onClick={handleNext}>Next</Link>
