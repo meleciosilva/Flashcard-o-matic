565f341c47b16bb434d18aca2a8e96f601fa4f91
meleciosilva <mpsilva94@gmail.com>
2021-03-17 11:29:28 -0400

    displays nav bar and card with title

---
+++ src/Study/Study.js
@@ -0,0 +1,24 @@
+import React from "react";
+import NavBar from "./NavBar";
+import DisplayCard from "./DisplayCard";
+import {useParams} from "react-router-dom";
+
+function Study({decks, cards}) {
+  const {deckId} = useParams();
+
+  // identifies selected deck to view
+  const deck = decks.find((item) => item.id === +deckId);
+
+  // identifies cards for selected deck
+  const deckCards = cards.filter(item => item.deckId === +deckId);
+  
+  return (
+  <>
+    <NavBar deck={deck}/>
+    <h1 style={{textAlign:"center"}}>{deck.name}</h1>
+    <DisplayCard deckCards={deckCards}/>
+  </>
+  )
+}
+
+export default Study;
