e6eaaf4a4e5a1a09953efd588af88b0356f5026a
meleciosilva <mpsilva94@gmail.com>
2021-03-19 21:37:59 -0400

    restructured routes

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,22 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Switch, Route} from "react-router-dom";
 import Header from "./Header";
 import NotFound from "./NotFound";
-import Home from "../Home/Home";
-
-// import Home from "../Home/Home";
+import Home from "../components/Home";
+import Deck from "../components/Deck/index";
+import Study from "../components/Study/Study";
+// import CreateDeck from "../components/CreateDeck/CreateDeck";
 
 function Layout() {
+    
   return (
     <>
       <Header />
       <div className="container">
-        {/* TODO: Implement the screen starting here */}
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
-          <Route>
+          {/* <Route exact path="/decks/:deckId">
+            <Deck />
+          </Route>
+          <Route path="/decks/:deckId/study">
+            <Study />
+          </Route>
+          <Route exact path="/decks/new">
+            <CreateDeck />
+          </Route> */}
+          <Route path="*">
             <NotFound />
           </Route>
         </Switch>
