09ac5826d8de1d53ab73572927b8ef137c56ab1e
meleciosilva <mpsilva94@gmail.com>
2021-03-21 19:05:47 -0400

    deleted unused imports

--- src/components/Home/ShowDecks.js
+++ src/components/Home/ShowDecks.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import {Link, Route, useHistory} from "react-router-dom";
 import { deleteDeck } from "./../../utils/api/index";
 import AddDeckButton from "./AddDeckButton";
