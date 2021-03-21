55a48cd84f8ab6a93fa84d52607ccc3610148893
meleciosilva <mpsilva94@gmail.com>
2021-03-21 19:05:21 -0400

    deleted Switch and Routes

--- src/components/Decks/Study/NotEnoughCards.js
+++ src/components/Decks/Study/NotEnoughCards.js
@@ -1,23 +1,14 @@
 import React from "react";
-import {Link, useParams, Switch, Route, useRouteMatch} from "react-router-dom";
-import AddCard from "./AddCard"
+import {Link, useParams} from "react-router-dom";
 
 function NotEnoughCards({deckCards}) {
   const {deckId} = useParams();
-  const {url} = useRouteMatch();
   
   return (
     <>
-      <Switch>
-        <Route >
-          <h1>Not Enough Cards</h1>
-          <p>You need at least 3 cards to study. You have {deckCards.length} cards in your deck.</p>
-          <Link to={`/decks/${deckId}/cards/new`} type="button" class="btn btn-primary"><strong>+ Add Cards</strong></Link>
-        </Route>
-        {/* <Route path={`/decks/:deckId/cards/new`}>
-          <AddCard />
-        </Route> */}
-      </Switch>
+      <h1>Not Enough Cards</h1>
+      <p>You need at least 3 cards to study. You have {deckCards.length} cards in your deck.</p>
+      <Link to={`/decks/${deckId}/cards/new`} type="button" class="btn btn-primary"><strong>+ Add Cards</strong></Link>
     </>
   )
 }
