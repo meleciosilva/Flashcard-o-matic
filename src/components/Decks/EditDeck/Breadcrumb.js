327ce16e00aec29e102444f8a1afec17738432ed
meleciosilva <mpsilva94@gmail.com>
2021-03-21 19:13:18 -0400

    updated Link url

--- src/components/Decks/EditDeck/Breadcrumb.js
+++ src/components/Decks/EditDeck/Breadcrumb.js
@@ -1,14 +1,13 @@
 import React from "react";
-import {Link, useRouteMatch} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function Breadcrumb({deck}) {
-  const {url} = useRouteMatch()
   
   return (
     <nav aria-label="breadcrumb">
     <ol className="breadcrumb">
       <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-      <li className="breadcrumb-item"><Link to={`${url}`}>{deck.name}</Link></li>
+      <li className="breadcrumb-item"><Link to={`/decks/${deck.id}`}>{deck.name}</Link></li>
       <li className="breadcrumb-item active" aria-current="page">Edit</li>
     </ol>
   </nav>
